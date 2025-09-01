1d4ca0846bb8d5c9bb37f72990332dcad7b70b7b
RehnumaTasnim71 <rehnumatasnim71@gmail.com>
2025-09-02 03:27:28 +0800

    ready

--- event_management/settings.py
+++ event_management/settings.py
@@ -8,7 +8,7 @@ DEBUG = True
 ALLOWED_HOSTS = ['*']
 CSRF_TRUSTED_ORIGINS = [
     "http://127.0.0.1:8000",
-    "https://event-management-82ih.onrender.com"
+    "https://*.onrender.com"
 ]
 INSTALLED_APPS = [
     'django.contrib.admin',
